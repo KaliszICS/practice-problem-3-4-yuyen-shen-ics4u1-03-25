4037c68249e33e956b1f94c19a6df31982bfc6c4
Yu-Yen, Shen <yu-yen.shen@student.tdsb.on.ca>
2025-04-04 19:50:40 +0000

    yuyen

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,48 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		System.out.println(recaman(6));
+	}
+
+	public static int[] recaman(int n) {
+
+		if (n == 1) {
+			return new int[]{n};
+		}
+		if (n <= 0){
+			return new int[]{};
+		}
+
+		int[] nums = new int[n];
+		nums[0] = 1;
+		nums[1] = 3;
+
+		recamanHelper (n, nums, 2);
+		return nums;
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static void recamanHelper (int n, int[] nums, int index) {
+		if (index >= n){
+			return;
+		}
+
+		int pre = nums[index - 1];
+		int next = pre - (index + 1);
+
+		if (next > 0 && !contains(nums, index, next)) {
+			nums[index] = next;
+		} else {
+			nums[index] = pre + index + 1;
+		}
+
+		recamanHelper(n, nums, index + 1);
 	}
 
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
+	private static boolean contains(int[] arr, int length, int value) {
+        for (int i = 0; i < length; i++) {
+            if (arr[i] == value) return true;
+        }
+        return false;
+    }
 }
